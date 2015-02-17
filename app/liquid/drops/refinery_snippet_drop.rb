15457c62c8e1463bfe27d3892dd20fd003e38066
Yaroslav Agarkov <ya.agarkov@i-sys.ru>
2015-02-17 18:28:45 +0500

    adding body parsing for liquid

--- app/liquid/drops/refinery_snippet_drop.rb
+++ app/liquid/drops/refinery_snippet_drop.rb
@@ -1,5 +1,11 @@
 class Refinery::Snippets::SnippetDrop < Clot::BaseDrop
 
-  self.liquid_attributes = [:created_at, :updated_at, :id, :title, :body, :page_parts]
+  self.liquid_attributes = [:created_at, :updated_at, :id, :title, :page_parts]
+
+  def body
+    return if @source.body.blank?
+    liquid = Liquid::Template.parse @source.body.html_safe
+    liquid.render(@context)
+  end
 
 end
